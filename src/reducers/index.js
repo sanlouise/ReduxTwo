43164a2f5dcff739e41d11d13dbe8568ca4c94bb
Sandra Adams-Hallie <sandra.adamshallie@gmail.com>
2016-06-16 14:05:21 +0200

    Create book list component

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,7 @@
 import { combineReducers } from 'redux';
 import BooksReducer from './reducer_books';
 
+//We tell app how to create application state
 const rootReducer = combineReducers({
   books: BooksReducer
 });
