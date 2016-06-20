235fa7d4767f210b92ed001104555af6e27c4490
Sandra Adams-Hallie <sandra.adamshallie@gmail.com>
2016-06-20 21:57:51 +0200

    Connect book-detail container to redux

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux';
 import BooksReducer from './reducer_books';
-import ActiveBook from '/reducer_active_book'
+import ActiveBookReducer from './reducer_active_book'
 
 //We tell app how to create application state
 const rootReducer = combineReducers({
--- src/reducers/reducer_active_book.js
+++ src/reducers/reducer_active_book.js
@@ -1,8 +1,9 @@
 
 //State argument is not application state, only state this reducer is responsible for
+//Null is to ensure that when no information is passed along, undefined cannot be returned
 export default function(state = null, action) {
 	switch(action.type) {
-		case: 'BOOK_SELECTED':
+		case 'BOOK_SELECTED':
 		return action.payload
 	}
 	return state
